4b6d71343cd639e5a156b73cabb3485c30569217
Charles Trangay <charles.trangay11@gmail.com>
2021-06-12 19:11:52 -0400

    added drawer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { AppBar, Tab, Tabs, Toolbar } from "@material-ui/core";
+import {
+	AppBar,
+	Tab,
+	Tabs,
+	Toolbar,
+	Hidden,
+	Box,
+	Drawer,
+	List,
+	ListItem,
+	ListItemText,
+	ListItemIcon,
+} from "@material-ui/core";
 import ContactsIcon from "@material-ui/icons/Contacts";
 import HomeIcon from "@material-ui/icons/Home";
 import BarChartIcon from "@material-ui/icons/BarChart";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "../utils/theme";
@@ -25,31 +38,68 @@ const sectionItems = [
 	},
 ];
 
+//To hide tabs just use display: none or MUI Hide
 export default function Navbar() {
+	const [drawerOpen, setDrawerOpen] = React.useState(false);
+	const toggleDrawer = () => {
+		setDrawerOpen(!drawerOpen);
+	};
 	return (
 		<div>
 			{/**ThemeProvider changes primary color or just add css styles */}
 			<ThemeProvider theme={theme}>
 				<AppBar color="primary" position="sticky">
 					<Toolbar>
-						<div style={{ margin: "auto" }}>
+						<Box margin={{ sm: "auto" }}>
 							<Tabs>
-								{sectionItems.map((sectionItem) => {
-									const { sectionTitle, pageUrl, icon } = sectionItem;
-									return (
-										<Tab
-											disableTouchRipple
-											icon={icon}
-											label={sectionTitle}
-											component={Link}
-											to={pageUrl}
-										/>
-									);
-								})}
-								;
+								<Hidden xsDown>
+									{sectionItems.map((sectionItem) => {
+										const { sectionTitle, pageUrl, icon } = sectionItem;
+										return (
+											<Tab
+												disableTouchRipple
+												icon={icon}
+												label={sectionTitle}
+												component={Link}
+												to={pageUrl}
+											/>
+										);
+									})}
+								</Hidden>
+								<Hidden smUp>
+									<Tab
+										disableTouchRipple
+										icon={<MenuIcon />}
+										onClick={toggleDrawer}
+									/>
+								</Hidden>
 							</Tabs>
-						</div>
+						</Box>
 					</Toolbar>
+					<Hidden smUp>
+						<Drawer
+							anchor="left"
+							variant="temporary"
+							onClose={toggleDrawer}
+							open={drawerOpen}
+						>
+							<List>
+								{sectionItems.map((props) => (
+									<ListItem
+										onClick={toggleDrawer}
+										button
+										key={props.sectionTitle}
+										component="a"
+										href={props.pageUrl}
+										primary={props.sectionTitle}
+									>
+										<ListItemIcon>{props.icon}</ListItemIcon>
+										<ListItemText>{props.sectionTitle}</ListItemText>
+									</ListItem>
+								))}
+							</List>
+						</Drawer>
+					</Hidden>
 				</AppBar>
 			</ThemeProvider>
 		</div>
