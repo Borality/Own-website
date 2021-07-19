e504f711c1043abc3830efeeea509323369bbfc4
Charles Trangay <charles.trangay11.com>
2021-07-19 11:30:57 -0400

    added new project

--- src/pages/projects/Data.js
+++ src/pages/projects/Data.js
@@ -1,6 +1,7 @@
 //Images
 import projectBrochure from "../../images/projectBrochure.PNG";
 import weCook from "../../images/weCook.PNG";
+import crudApp from "../../images/crudApp.PNG"
 //Stores data of the cards
 export default [
 	{
@@ -18,8 +19,16 @@ export default [
 		title: "WeCook website",
 		description:
 			"Full stack application built using React and other frameworks/libraries that utilized API's and a database for the backend. It was a group project. I mainly focused on creating the frontend part of the application and the database to an extent.",
-		button: "Website",
 		hrefWebsite: "https://github.com/Borality/WeCook",
 		hrefCode: "https://github.com/Borality/WeCook",
+	},
+	{
+		image: crudApp,
+		alt: "crud app picture",
+		title: "CRUD website",
+		description: "A basic CRUD application using React, Material UI, MYSQL, ExpressJS.",
+		button: "Website",
+		hrefWebsite: "https://github.com/Borality/CRUD-APP",
+		hrefCode: "https://github.com/Borality/CRUD-APP",
 	},
 ];
